e9ca6f0c05665d7d15e3ba2ca50772facf8ac73c
GhostCoder <mhtbtanvir01@gmail.com>
2025-06-27 22:52:08 +0600

    backend init

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import bodyparser from "body-parser";
+import morgan from "morgan";
+/*Routes*/
+
+/*configureations*/
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+/*Routes*/
+app.get("/hello", (req, res) => {
+    res.send("Hello World!");
+})
+/*Server*/
+const port = process.env.PORT||3001;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
